90c3b3dcb36a71565397468d30ce0acab1a8dd41
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-02-08 23:30:19 -0300

    criada conexões com rotas de votos, enquetes e criação

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,14 +1,19 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod';
+import cookie from "@fastify/cookie"
 import { createPoll } from "../routes/create-poll";
-
+import { getPoll } from "../routes/get-poll";
+import { voteOnPoll } from "../routes/vote_on_poll";
 
 const app = fastify();
 
+app.register(cookie, {
+    secret: "my-secret",
+    hook: "onRequest"
+})
 
-
-
+app.register(createPoll);
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({port: 3333}).then(() => {
     console.log(`HTTP server running`)
